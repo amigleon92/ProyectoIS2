dd8922898bbd0f1eff0cbeb61721b963be570c7d
amigleon92 <amigleon92@gmail.com>
2014-04-09 16:07:36 -0400

    Pagina inicio

--- ProjectApp/views.py
+++ ProjectApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views.generic import TemplateView
 
-# Create your views here.
+class inicio(TemplateView):
+    template_name = 'inicio.html'
+
