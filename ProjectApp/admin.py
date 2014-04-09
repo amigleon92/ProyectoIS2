d3c6a25099bb0c779a053f3cbf239045b9db4e6f
amigleon92 <amigleon92@gmail.com>
2014-04-09 17:07:09 -0400

    Pagina inicio

--- ProjectApp/admin.py
+++ ProjectApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from ProjectApp.models import Usuarios
+admin.site.register(Usuarios)
--- ProjectApp/models.py
+++ ProjectApp/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Usuarios(models.Model):
+    nick= models.CharField(max_length=15)
+    nombre= models.CharField(max_length=50)
+    apellido= models.CharField(max_length=50)
+    cedula= models.PositiveIntegerField()
+    email= models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.nick
