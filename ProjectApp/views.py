1fbcbf4a1a82a248a9ff0e4325f3c36b61df7ee1
amigleon92 <amigleon92@gmail.com>
2014-04-10 22:47:08 -0400

    Login exitoso

--- ProjectApp/views.py
+++ ProjectApp/views.py
@@ -1,30 +1,18 @@
-from django.http import request
 from django.views.generic import TemplateView, CreateView, ListView
 from ProjectApp.models import Usuarios
 from django.core.urlresolvers import reverse_lazy
-from django.shortcuts import render, render_to_response
-
 
 class login(TemplateView):
     template_name = 'login.html'
 
-
 class inicio(TemplateView):
-    def post(self, request, *args, **kwargs):
-        buscar_user = request.POST['user']
-        buscar_password = request.POST['pass']
-        for i in Usuarios.objects.all():
-            if i.nick == buscar_user and i.password == buscar_password:
-                return render(request, 'inicio.html')
-        return render(request, 'login.html')
+    template_name = "inicio.html"
 
 class RegistrarUsuario(CreateView):
-    template_name = 'CrearUsuario.html'
+    template_name = "CrearUsuario.html"
     model = Usuarios
     success_url = reverse_lazy('listar_usuario')
 
-
-class ListarUsuario(TemplateView):
-    def post(self, request, *args, **kwargs):
-
-        return render(request, 'Usuario.html')
+class ListarUsuario(ListView):
+    template_name = 'Usuario.html'
+    model = Usuarios
