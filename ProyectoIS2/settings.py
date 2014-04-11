f3036c804340c6402b4493d013e56a0a3fe90c8c
Reny <rrf.marin@gmail.com>
2014-04-10 20:57:03 -0400

    estilos de login 2.0

--- ProyectoIS2/settings.py
+++ ProyectoIS2/settings.py
@@ -85,5 +85,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATICFILES_DIRS= (PROJECT_PATH.child('static'),)
 STATIC_URL = '/static/'
