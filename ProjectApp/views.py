e1c66ce8d11544072132247dd1a527818de8239b
amigleon92 <amigleon92@gmail.com>
2014-04-11 03:12:10 -0400

    Login, crear usuario, volver a los menus anteriores...

--- ProjectApp/views.py
+++ ProjectApp/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render, render_to_response
 
 class login(TemplateView):
     template_name = 'login.html'
+    def get(self, request, *args, **kwargs):
+        return render(request,'login.html')
 
 
 class inicio(TemplateView):
@@ -17,14 +19,28 @@ class inicio(TemplateView):
             if i.nick == buscar_user and i.password == buscar_password:
                 return render(request, 'inicio.html')
         return render(request, 'login.html')
+    def get(self, request, *args, **kwargs):
+        return render(request, 'inicio.html')
 
-class RegistrarUsuario(CreateView):
-    template_name = 'CrearUsuario.html'
-    model = Usuarios
-    success_url = reverse_lazy('listar_usuario')
+class RegistrarUsuario(TemplateView):
+    def post(self, request, *args, **kwargs):
+        return render(request, 'CrearUsuario.html')
 
 
 class ListarUsuario(TemplateView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):   #insertamos un usuario en la tabla
+        new_user= request.POST['user']          #lo que nos devuelve el html en 'user'
+        new_nombre= request.POST['nombre']
+        new_apellido= request.POST['apellido']
+        new_email= request.POST['email']
+        new_cedula= request.POST['cedula']
+        new_password= request.POST['pass']
+        nuevo_usuario= Usuarios(nick= new_user, nombre= new_nombre, apellido= new_apellido, email= new_email, cedula= new_cedula, password= new_password) #insert into values
+        nuevo_usuario.save()                    #guardamos en la base de datos
+        lista= Usuarios.objects.all()
+        return render(request, 'Usuario.html', {   'lista_usuarios':lista})     #enviamos al html un diccionario que tiene par 'key':lista_de_Usuarios_Existentes
+    def get(self, request, *args, **kwargs):
+        lista= Usuarios.objects.all()
+        return render(request, 'Usuario.html', {'lista_usuarios':lista})
 
-        return render(request, 'Usuario.html')
+
