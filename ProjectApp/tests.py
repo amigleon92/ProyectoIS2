7cd44e09a8a5cc8b2f394ccbb4661e73830809ca
choffix <seorue@gmail.com>
2014-04-12 05:13:29 -0400

    documentacion y testing

--- ProjectApp/tests.py
+++ ProjectApp/tests.py
@@ -1,9 +1,16 @@
 from unittest import TestCase
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
 class TestLogin(TestCase):
-    def test_get(self):
-        self.fail()
+    def setUp(self):
+        self.client = Client()
+        self.username = 'prueba'
+        self.email = 'test@test.com'
+        self.password = 'prueba'
+        self.test_user = User.objects.create_user(self.username, self.email, self.password)
 
+    def test_login_exitoso(self):
+        login = self.client.login(username=self.username, password=self.password)
+        self.assertEqual(login, True)
 
