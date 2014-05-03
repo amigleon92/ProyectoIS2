3c9d26f7eeb3bc8dd09dff1d4e10d1bc12934b59
choffix <seorue@gmail.com>
2014-05-02 20:31:23 -0400

    Documentacion y testing

--- ProjectApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from ProjectApp.models import Usuarios, Roles, Proyecto, Fase
-admin.site.register(Usuarios)
-admin.site.register(Roles)
-admin.site.register(Proyecto) #para que muestre en la administracion el modelo proyecto
-admin.site.register(Fase)
--- source/models.rst
+++
@@ -1,4 +0,0 @@
-Models
-================
-.. automodule:: ProjectApp.models
-    :members:
