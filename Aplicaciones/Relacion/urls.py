2b048b9e50e4454143fd57500e01942c1c32f7df
renymarin <rrf.marin@gmail.com>
2014-05-17 01:56:31 -0400

    Establecer relacion antecesor / sucesor

--- Aplicaciones/Relacion/urls.py
+++ Aplicaciones/Relacion/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-from .views import RelacionView
+from .views import RelacionView, EstablecerRelacionAS, EstablecerRelacionASConfirm
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns= patterns('',
     url(r'^$', RelacionView.as_view(), name="item"),
-
+    url(r'^establecerRelacionAS/$', EstablecerRelacionAS.as_view(), name="establecerRelacionAS"),
+    url(r'^establecerRelacionAS/Confirmar/$', EstablecerRelacionASConfirm.as_view(), name="establecerRelacionASConfirm")
 
     )
