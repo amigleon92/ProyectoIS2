3db28953892808c3b4f53b8851dd1a56ceea99c4
amigleon92 <amigleon92@gmail.com>
2014-05-23 19:09:59 -0400

    Generar Solicitud en Completar Atributo

--- Aplicaciones/Atributo/views.py
+++ Aplicaciones/Atributo/views.py
@@ -8,6 +8,7 @@ from Aplicaciones.Tipo_de_Atributo.models import Tipo_de_Atributo
 from Aplicaciones.Item.models import Item
 from Aplicaciones.Item.views import ItemView
 from Aplicaciones.Fase.models import Fase
+from Aplicaciones.Solicitud_de_Cambios.models import Solicitud_de_Cambios, Voto
 # Create your views here.
 
 class AtributoView(ItemView):
@@ -151,11 +152,22 @@ class CompletarAtributo(AtributoView):
         diccionario['atributo']=atributo_actual
         diccionario['item']=item_actual
         if len(Rol.objects.filter(usuario=usuario_logueado, proyecto=proyecto_actual,completar_atributos=True, activo=True)):
-           if item_actual.estado=='A':
+            if item_actual.estado=='A':
                 diccionario['lista_atributos']= (Atributo.objects.filter(item= item_actual, activo=True)).order_by('nombre')
                 diccionario['error']= 'Item Aprobado. No puedes realizar esta accion'
                 return render(request, super(CompletarAtributo, self).template_name, diccionario)
-           return render(request, self.template_name, diccionario)
+            elif item_actual.estado == 'B':
+                #Si se definio ya los miembros del Comite.
+                if len(Rol.objects.filter(nombre= 'Miembro del Comite', proyecto=proyecto_actual)):
+                    diccionario['aviso']= 'Los cambios que realizara entraran en una Solicitud de Cambios'
+                    return render(request, self.template_name, diccionario)
+                else:
+                    diccionario['lista_items']= (Item.objects.filter(fase= fase_actual, activo=True)).order_by('nombre')
+                    diccionario['error']= 'Aun no se definio un Comite de Cambios - No puede realizar esta accion'
+                    return render(request, super(CompletarAtributo, self).template_name, diccionario)
+            else:
+                #El item es desaprobado
+                return render(request, self.template_name, diccionario)
         else:
             diccionario['lista_atributos']= (Atributo.objects.filter(item= item_actual, activo=True)).order_by('nombre')
             diccionario['error']= 'No puedes realizar esta accion'
@@ -175,25 +187,68 @@ class CompletarAtributoConfirm(CompletarAtributo):
         diccionario['item']=item_actual
         diccionario['fase']= fase_actual
         diccionario['proyecto']= proyecto_actual
-
-        #Guardamos la version anterior
-        version_anterior= self.crear_copia(item_actual)
-        version_anterior.activo= False
-        version_anterior.save()
-        #Actualizamos la version
-        item_actual.version+=1 #Atributo Completado NuevaVersion
-        item_actual.version_descripcion='Atributo '+ atributo_actual.nombre + ' completado'
-        item_actual.save()
-        #Completamos Atributo
-        if atributo_actual.tipo_de_atributo_tipo == 'N':
-            atributo_actual.tipo_numerico= request.POST['tipo_numerico']
-        elif atributo_actual.tipo_de_atributo_tipo == 'T':
-            atributo_actual.tipo_texto= request.POST['tipo_texto']
-        elif atributo_actual.tipo_de_atributo_tipo == 'B':
-            if 'tipo_buleano' in request.POST: atributo_actual.tipo_boolean= True
-            else: atributo_actual.tipo_boolean= False
-        elif atributo_actual.tipo_de_atributo_tipo == 'F':
-            atributo_actual.tipo_fecha=request.POST['tipo_fecha']
-        atributo_actual.save()
+        print item_actual.estado
+        if item_actual.estado=='D':
+            #Guardamos la version anterior
+            version_anterior= self.crear_copia(item_actual)
+            version_anterior.activo= False
+            version_anterior.save()
+            #Actualizamos la version
+            item_actual.version+=1 #Atributo Completado NuevaVersion
+            item_actual.version_descripcion='Atributo '+ atributo_actual.nombre + ' completado'
+            item_actual.save()
+            #Completamos Atributo
+            if atributo_actual.tipo_de_atributo_tipo == 'N':
+                atributo_actual.tipo_numerico= request.POST['tipo_numerico']
+            elif atributo_actual.tipo_de_atributo_tipo == 'T':
+                atributo_actual.tipo_texto= request.POST['tipo_texto']
+            elif atributo_actual.tipo_de_atributo_tipo == 'B':
+                if 'tipo_buleano' in request.POST: atributo_actual.tipo_boolean= True
+                else: atributo_actual.tipo_boolean= False
+            elif atributo_actual.tipo_de_atributo_tipo == 'F':
+                atributo_actual.tipo_fecha=request.POST['tipo_fecha']
+            atributo_actual.save()
+        else:
+            #Versiones de la solicitud
+            version_desaprobada= item_actual
+            version_desaprobada.estado= 'R'
+            version_desaprobada.save()
+            version_aprobada= self.crear_copia(item_actual)
+            version_aprobada.version=item_actual.version + 1
+            version_aprobada.version_descripcion= 'Atributo Agregado'
+            version_aprobada.activo= False
+            version_aprobada.save()
+            #Completamos Atributo
+            if atributo_actual.tipo_de_atributo_tipo == 'N':
+                atributo_actual.tipo_numerico= request.POST['tipo_numerico']
+            elif atributo_actual.tipo_de_atributo_tipo == 'T':
+                atributo_actual.tipo_texto= request.POST['tipo_texto']
+            elif atributo_actual.tipo_de_atributo_tipo == 'B':
+                if 'tipo_buleano' in request.POST: atributo_actual.tipo_boolean= True
+                else: atributo_actual.tipo_boolean= False
+            elif atributo_actual.tipo_de_atributo_tipo == 'F':
+                atributo_actual.tipo_fecha=request.POST['tipo_fecha']
+            atributo_actual.item= version_desaprobada
+            atributo_actual.save()
+            #Generamos la Solicitud de Cambios
+            nueva_solicitud= Solicitud_de_Cambios(
+                descripcion= 'Editar Item ' + version_desaprobada.nombre,
+                costo_del_impacto= 1000,
+                proyecto= proyecto_actual,
+                fase= fase_actual,
+                item_sc_aprobado= version_aprobada,
+                item_sc_desaprobado= version_desaprobada,
+            )
+            nueva_solicitud.save()
+            #Generamos los votos
+            for miembro in Rol.objects.filter(nombre= 'Miembro del Comite', proyecto= proyecto_actual, activo= True):
+                nuevo_voto= Voto(
+                    usuario= miembro.usuario,
+                    solicitud_de_cambios= nueva_solicitud,
+                )
+                nuevo_voto.save()
+            #Rompemos la Linea Base
+            item_actual.lineaBase.estado= 'A'
+            item_actual.lineaBase.save()
         return render(request, self.template_name, diccionario)
 
