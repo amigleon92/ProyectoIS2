3a79932cc3cbf8c1fc99d58ab65d5df4c95cb9a8
renymarin <rrf.marin@gmail.com>
2014-06-03 18:14:59 -0400

    contar votos

--- Aplicaciones/Solicitud_de_Cambios/views.py
+++ Aplicaciones/Solicitud_de_Cambios/views.py
@@ -7,6 +7,7 @@ from Aplicaciones.Proyecto.models import Proyecto
 from Aplicaciones.Item.models import Item
 from Aplicaciones.Item.views import ItemView
 from Aplicaciones.Fase.models import Fase
+from Aplicaciones.Proyecto.views import ProyectoView
 
 
 # Create your views here.
@@ -23,8 +24,19 @@ class SolicitudesDeFaseview(ItemView):
         diccionario['fase']=fase_actual
         diccionario['lista_solicitudes']= Solicitud_de_Cambios.objects.filter(proyecto=proyecto_actual, fase=fase_actual, activo=True)
         return render(request, self.template_name, diccionario)
-
-
+    def ContarVotos(self, solicitud_actual):
+        if not len(Voto.objects.filter(solicitud_de_cambios=solicitud_actual, voto='Pe')):
+            si=0
+            no=0
+            votos_de_solicitud=Voto.objects.filter(solicitud_de_cambios=solicitud_actual)
+            for voto_solicitud in votos_de_solicitud:
+                if voto_solicitud.voto == 'Si': si=si+1
+                elif voto_solicitud.voto == 'No': no=no+1
+            if si>no:
+                solicitud_actual.estado='A'
+            else:
+                solicitud_actual.estado='D'
+            solicitud_actual.save()
 
 class VotarFase(SolicitudesDeFaseview):
     template_name = 'Solicitud_de_Cambios/VotarFase.html'
@@ -40,7 +52,6 @@ class VotarFase(SolicitudesDeFaseview):
         diccionario['fase']=fase_actual
         if len(Rol.objects.filter(nombre='Miembro del Comite', usuario=usuario_logueado, activo=True)):
             voto_miembro=Voto.objects.filter(usuario=usuario_logueado, solicitud_de_cambios=solicitud_actual, activo=True)
-            #print(voto_miembro[0].voto)
             if not voto_miembro[0].voto=='Pe':
                 diccionario['lista_solicitudes']= Solicitud_de_Cambios.objects.filter(proyecto=proyecto_actual, fase=fase_actual, activo=True)
                 diccionario['error']= 'Usted ya ha votado.'
@@ -68,4 +79,50 @@ class VotarFaseConfirm(VotarFase):
         voto_miembro.save()
         solicitud_actual.cantidad_de_votos=solicitud_actual.cantidad_de_votos+1
         solicitud_actual.save()
+        self.ContarVotos(solicitud_actual)
+        return render(request, self.template_name, diccionario)
+
+class SolicitudesDeProyectoView(ProyectoView):
+    template_name = 'Solicitud_de_Cambios/SolicitudesDeProyecto.html'
+    def post(self, request, *args, **kwargs):
+        diccionario={}
+        usuario_logueado= Usuario.objects.get(id= request.POST['login'])
+        diccionario['logueado']= usuario_logueado
+        lista_solicitudes=[]
+        for miembro in Rol.objects.filter(nombre='Miembro del Comite', usuario=usuario_logueado):
+              if len(Solicitud_de_Cambios.objects.filter(proyecto=miembro.proyecto, estado='V', activo=True)):
+                   for solicitud in Solicitud_de_Cambios.objects.filter(proyecto=miembro.proyecto,estado='V', activo=True):
+                       voto_actual=Voto.objects.filter(usuario=usuario_logueado, solicitud_de_cambios=solicitud)
+                       if voto_actual[0].voto == 'Pe':
+                            lista_solicitudes.append(solicitud)
+        diccionario['lista_solicitudes']=lista_solicitudes
+        return render(request, self.template_name, diccionario)
+
+class VotarProyecto(SolicitudesDeProyectoView):
+    template_name = 'Solicitud_de_Cambios/VotarProyecto.html'
+    def post(self, request, *args, **kwargs):
+        diccionario={}
+        usuario_logueado= Usuario.objects.get(id= request.POST['login'])
+        solicitud_actual=Solicitud_de_Cambios.objects.get(id=request.POST['solicitud'])
+        diccionario['solicitud']=solicitud_actual
+        diccionario['logueado']= usuario_logueado
+        voto_miembro=Voto.objects.filter(usuario=usuario_logueado, solicitud_de_cambios=solicitud_actual, activo=True)
+        diccionario['voto_miembro']=voto_miembro
+        return render(request, self.template_name, diccionario)
+
+class VotarProyectoConfirm(VotarProyecto):
+    template_name = 'Solicitud_de_Cambios/VotarProyectoConfirm.html'
+    def post(self, request, *args, **kwargs):
+        diccionario={}
+        usuario_logueado= Usuario.objects.get(id= request.POST['login'])
+        solicitud_actual=Solicitud_de_Cambios.objects.get(id=request.POST['solicitud'])
+        voto_actual=request.POST['voto']
+        voto_miembro=Voto.objects.get(usuario=usuario_logueado, solicitud_de_cambios=solicitud_actual, activo=True)
+        diccionario['logueado']= usuario_logueado
+        voto_miembro.voto=voto_actual
+        voto_miembro.save()
+        solicitud_actual.cantidad_de_votos=solicitud_actual.cantidad_de_votos+1
+        solicitud_actual.save()
+        contar= SolicitudesDeFaseview()
+        contar.ContarVotos(solicitud_actual)
         return render(request, self.template_name, diccionario)
